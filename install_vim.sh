e155b23cf674bfe7f3345ae6360217bcc666d6ea
jayden <jayden717@gmail.com>
2016-03-30 23:00:01 +0800

    add install_vim.sh

---
+++ install_vim.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+mkdir -p ~/vim_tools
+cd ~/vim_tools
+
+wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+cp -r dircolors.ansi-dark ~/.dircolors
+eval `dircolors ~/.dircolors`
+
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./set_dark.sh
+#./set_light.sh
+
+cd 
+
+#Set up Vundle:
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
+
